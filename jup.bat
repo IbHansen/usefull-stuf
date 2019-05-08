b54c2c601a8f17daeb894f68008afde32ac94ea0
IbHansen <43575358+IbHansen@users.noreply.github.com>
2019-05-08 14:29:21 +0200

    useful bat files for Jupyter

---
+++ jup.bat
@@ -0,0 +1,1 @@
+cmd /K "C:\Anaconda3\Scripts\activate.bat C:\Anaconda3  && jupyter notebook "
---
+++ show.bat
@@ -0,0 +1,1 @@
+jupyter nbconvert "%1.ipynb" --to slides --post serve  --SlidesExporter.reveal_scroll=True
