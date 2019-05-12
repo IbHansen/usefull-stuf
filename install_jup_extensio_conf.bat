4a751b11d79ff859b817a1eacdeff19bcd6f1fc9
IbHansen <43575358+IbHansen@users.noreply.github.com>
2019-05-12 12:10:31 +0200

    Update install_jup_extensio_conf.bat

--- install_jup_extensio_conf.bat
+++ install_jup_extensio_conf.bat
@@ -1,3 +1,3 @@
 rem install Jupyter extensions 
 conda install -c conda-forge jupyter_contrib_nbextensions
-condya install -c conda-forge jupyter_nbextensions_configurator
+conda install -c conda-forge jupyter_nbextensions_configurator
